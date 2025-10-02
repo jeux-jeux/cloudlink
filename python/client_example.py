aa1c576410a535672b73b4185a9bc4177f365619
jeux-jeux <danielersen@icloud.com>
2025-10-02 07:52:24 +0200

    Update client_example.py

--- python/client_example.py
+++ python/client_example.py
@@ -157,5 +157,5 @@ def route_private_variable():
 
 # ---------- Start ----------
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", "5000"))
+    port = int(os.getenv("PORT", "3000"))
     app.run(host="0.0.0.0", port=port)
